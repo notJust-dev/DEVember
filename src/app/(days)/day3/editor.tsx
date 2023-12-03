0bbde8f61ec7338854726c6931466b6e71f32ca7
Vadim Savin <savinvadim1312@gmail.com>
2023-12-03 13:17:35 +0000

    add a more complete MD template

--- src/app/(days)/day3/editor.tsx
+++ src/app/(days)/day3/editor.tsx
@@ -1,18 +1,50 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  TextInput,
-  Pressable,
-} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Pressable } from 'react-native';
 import React, { useState } from 'react';
-import Markdown from 'react-native-markdown-display';
 import MarkdownDisplay from '@components/day3/MarkdownDisplay';
 
-const template = `# Markdown editor
+const template = `# 🎉 Fun with Markdown!
 
-Hello **World**!
+## 🚀 Introduction
+Welcome to this **fun and exciting** markdown guide! Let's dive into the world of Markdown and discover how it makes text formatting cool and easy!
+
+## 🎈 Basics: Add Some Flair
+
+- **Bold and Beautiful:** Make your text stand out! Use \`**\` or \`__\`. Example: **Look at me!**
+- *Sassy Italics:* Add a slant with \`*\` or \`_\`. Example: *I'm leaning!*
+
+### 🍔 Let's List Some Fun Things!
+
+1. 🌟 Star gazing
+2. 🏖 Beach parties
+3. 🍕 Pizza nights
+
+- 🎮 Video games
+- 📚 Reading a good book
+- 🧘 Yoga time
+
+## 🌈 Advanced Fun
+
+### 🖼 Adding Images and Links
+
+A cute pic: 
+
+![Cute Cat](https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/6.jpg)
+
+Visit a fun site: [Fun Site](https://example.com)
+
+### 🎶 Code Block Party
+
+\`\`\`javascript
+// JavaScript party trick
+function partyTime() {
+    console.log("Let's dance 💃🕺!");
+}
+\`\`\`
+
+## 🎤 Conclusion
+Markdown is not just for formatting; it's for having fun while expressing yourself! Keep exploring and enjoy the markdown party! 🎊
+
+> Enjoy crafting your own fun markdown documents! 🎨🎉
 `;
 
 const EditorScreen = () => {
